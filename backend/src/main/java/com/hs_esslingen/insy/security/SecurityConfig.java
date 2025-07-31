514c9c2584754a8dfd61aaa44a5e831e5b3aa875
Thr0in <xthroin@gmail.com>
2025-07-31 11:04:36 +0200

    Enhance JWT role extraction in SecurityConfig
    Added custom AuthoritiesConverter and JwtAuthenticationConverter beans to extract roles from JWT claims, enabling more flexible role-based authorization. Updated securityFilterChain to use the new authentication converter and adjusted request matchers for improved access control.

--- backend/src/main/java/com/hs_esslingen/insy/security/SecurityConfig.java
+++ backend/src/main/java/com/hs_esslingen/insy/security/SecurityConfig.java
@@ -1,21 +1,31 @@
 package com.hs_esslingen.insy.security;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -26,7 +36,8 @@ import org.springframework.web.cors.CorsConfiguration;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+            Converter<Jwt, AbstractAuthenticationToken> authenticationConverter) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
@@ -37,12 +48,12 @@ public class SecurityConfig {
                     return corsConfiguration;
                 }))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/orders").hasAnyRole("SYSTEM") // Only allow BeSy to create
-                                                                                          // orders
-                        .anyRequest().authenticated())
+                        // Allow BeSy to create orders
+                        .requestMatchers("/orders/**").hasAnyRole("SYSTEM")
+                        .anyRequest().hasAuthority("insy"))
                 .httpBasic(Customizer.withDefaults()) // Enable HTTP Basic authentication for BeSy-API
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtDecoder -> jwtDecoder.jwtAuthenticationConverter(authenticationConverter)));
         return http.build();
     }
 
@@ -67,5 +78,29 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Converter to extract roles from JWT claims
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {
+    }
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+            var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+            return roles.map(List::stream)
+                    .orElse(Stream.empty())
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast)
+                    .toList();
+        };
+    }
+
+    @Bean
+    JwtAuthenticationConverter authenticationConverter(AuthoritiesConverter authoritiesConverter) {
+        var authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
+        return authenticationConverter;
     }
 }
